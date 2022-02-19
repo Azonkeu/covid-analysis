57ba41c5f08d5c4029d321cd04e5398a33e09f82
Azonkeu <mazonkeu@gmail.com>
2022-02-19 01:47:21 +0100

    Fix linters

--- src/__tests__/Homepage.test.js
+++ src/__tests__/Homepage.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import HomePage from '../components/HomePage';
+import HomePage from '../components/Homepage';
 import store from '../redux/ConfigureStore';
 
 describe('test for the HomePage', () => {
